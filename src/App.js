8d3ce513c59e270b3dc6d91c4817a462f849e8ee
Neil Hanson <pro.neilhanson@gmail.com>
2022-10-12 14:17:35 -0500

    added notes to key functions

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ import MatchSetup from "./components/MatchSetup";
 
 
 function App() {
+  // Local State
   const [gameCacheSettings, setGameCacheSettings] = useState(null);
 
+  // When app loads call getCache().
   useEffect(() => {
     getCache()
   },[])
 
+  /* 
+    --- getCache Function ---
+    1. Check to see if there is already a cache named "Game_Settings_Cache", create one if not.
+    2. Then async request a response for the url of "http://localhost:3000/" within the opened cache.
+    3. Await the response from the request, and then set our "GameCacheSettings" state with the response.
+  */
   const getCache = () => {
     caches
       .open('Game_Settings_Cache')
@@ -28,6 +36,7 @@ function App() {
   
   return (
     <div className="App">
+      {/* Pass "getCache" function as a prop to the "MatchSetup" component. */}
       <MatchSetup getCache={getCache}/>
       {gameCacheSettings && 
         <h3>This is the current game format: {gameCacheSettings.format} with {gameCacheSettings.playerCount} players.</h3>
--- src/components/MatchSetup.jsx
+++ src/components/MatchSetup.jsx
@@ -1,18 +1,27 @@
+// Imports
 import { useState } from 'react';
 
 function CreateMatch({getCache}) {
+    // Local State
     const [format, setFormat] = useState('Commander');
     const [playerCount, setPlayerCount] = useState(2);
 
+    // gameInfo Object to save into the cache.
     const gameInfo = {
         format,
         playerCount
     }
 
+    /*
+        --- confirmGameSettings Function ---
+        1. Async function that takes parameters of a cacheName, url, and response.
+        2. The response parameter is converted into an instance of a Response Object.
+        3. Check to see if there is a "Game_Settings_Cache", create one if not.
+        4. Use .put() to add data to the selected "Game_Settings_Cache".
+        5. Call "getCache" function prop to update "gameCacheSettings" state.
+    */
     async function confirmGameSettings(cacheName, url, response) {
-
         const data = new Response(JSON.stringify(response));
-
         await caches.open(cacheName)
             .then((cache) => {
                 cache.put(url, data)
